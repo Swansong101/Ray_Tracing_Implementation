563ddd19cffebab0c70db385b6b0e0576e187b9f
r-mbete <rubymbetyyy@gmail.com>
2023-11-20 13:45:40 +0300

    Second commit

--- main.py
+++ main.py
@@ -1,16 +1,192 @@
-# This is a sample Python script.
+import turtle
+import math
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+wn = turtle.Screen()
+wn.bgcolor("black")
+wn.title("Maze Game")
+wn.setup(700,700)
+
+#Register shapes
+turtle.register_shape("player_right.gif")
+turtle.register_shape("player_left.gif")
+turtle.register_shape("treasure.gif")
+turtle.register_shape("wall.gif")
+
+#Create Pen
+class Pen(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("green")
+        self.penup()
+        self.speed(0)
+
+class Player(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("player_right.gif")
+        self.color("white")
+        self.penup()
+        self.speed(0)
+        self.gold = 0
+
+    def go_up(self):
+        #Calculate the space to move to
+        move_to_x = player.xcor()
+        move_to_y = player.ycor() + 24
+
+        #Check if the space has a wall
+        if (move_to_x, move_to_y) not in walls:
+            self.goto(move_to_x, move_to_y)
+
+    def go_down(self):
+        # Calculate the space to move to
+        move_to_x = player.xcor()
+        move_to_y = player.ycor() - 24
+
+        # Check if the space has a wall
+        if (move_to_x, move_to_y) not in walls:
+            self.goto(move_to_x, move_to_y)
+
+    def go_left(self):
+        # Calculate the space to move to
+        move_to_x = player.xcor() - 24
+        move_to_y = player.ycor()
+
+        self.shape("player_left.gif")
+
+        # Check if the space has a wall
+        if (move_to_x, move_to_y) not in walls:
+            self.goto(move_to_x, move_to_y)
+
+    def go_right(self):
+        # Calculate the space to move to
+        move_to_x = player.xcor() + 24
+        move_to_y = player.ycor()
+
+        self.shape("player_right.gif")
+
+        # Check if the space has a wall
+        if (move_to_x, move_to_y) not in walls:
+            self.goto(move_to_x, move_to_y)
+
+    def is_collision(self, other):
+        a = self.xcor()-other.xcor()
+        b = self.ycor()-other.ycor()
+        distance = math.sqrt((a ** 2) + (b ** 2))
+
+        if distance < 5:
+            return True
+        else:
+            return False
+
+class Treasure(turtle.Turtle):
+    def __init__(self, x, y):
+        turtle.Turtle.__init__(self)
+        self.shape("treasure.gif")
+        self.color("gold")
+        self.penup()
+        self.speed(0)
+        self.gold = 100
+        self.goto(x, y)
+
+    def destroy(self):
+        self.goto(2000, 2000)
+        self.hideturtle()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#Create levels list
+level = [""]
+
+#Define first level
+level_1 = [
+    "XXXXXXXXXXXXXXXXXXXXXXXXX",
+    "XP XXXXXXX          XXXXX",
+    "X  XXXXXXX  XXXXXX  XXXXX",
+    "X       XX  XXXXXX  XXXXX",
+    "X       XX  XXX        XX",
+    "XXXXXX  XX  XXX        XX",
+    "XXXXXX  XX  XXXXXX  XXXXX",
+    "XXXXXX  XX    XXXX  XXXXX",
+    "X  XXX        XXXXT XXXXX",
+    "X  XXX  XXXXXXXXXXXXXXXXX",
+    "X         XXXXXXXXXXXXXXX",
+    "X                XXXXXXXX",
+    "XXXXXXXXXXXX     XXXXX  X",
+    "XXXXXXXXXXXXXXX  XXXXX  X",
+    "XXX  XXXXXXXXXX         X",
+    "XXX                     X",
+    "XXX         XXXXXXXXXXXXX",
+    "XXXXXXXXXX  XXXXXXXXXXXXX",
+    "XXXXXXXXXX              X",
+    "XX   XXXXX              X",
+    "XX   XXXXXXXXXXXXX  XXXXX",
+    "XX    XXXXXXXXXXXX  XXXXX",
+    "XX           XXX        X",
+    "XXXX                    X",
+    "XXXXXXXXXXXXXXXXXXXXXXXXX"
+]
+
+#Add a treasure list
+treasures = []
+
+#Add maze to mazes list
+level.append(level_1)
+
+#Create level setup function
+def setup_maze(level):
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            #Get the character at each x,y coordinate
+            character = level[y][x]
+            #Calculate the screen x,y coordinates
+            screen_x = -288 + (x * 24)
+            screen_y = 288 - (y * 24)
+            #Check if it an X
+            if character =="X":
+                pen.goto(screen_x, screen_y)
+                pen.shape("wall.gif")
+                pen.stamp()
+                #Add coordinates to wall list
+                walls.append((screen_x, screen_y))
+
+            if character == "P":
+                player.goto(screen_x, screen_y)
+
+            if character == "T":
+                treasures.append(Treasure(screen_x, screen_y))
+
+#Create class instances
+pen = Pen()
+player = Player()
+
+#Create wall coordinates
+walls = []
+
+#Set up the level
+setup_maze(level[1])
+print (walls)
+
+#Keyboard Binding
+turtle.listen()
+turtle.onkey(player.go_left,"Left")
+turtle.onkey(player.go_right,"Right")
+turtle.onkey(player.go_up," Up")
+turtle.onkey(player.go_down,"Down")
+
+#Turn off screen updates
+wn.tracer(0)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#Main game loop
+while True:
+    #Iterate through treasure list
+    for treasure in treasures:
+        if player.is_collision(treasure):
+            player.gold += treasure.gold
+            print ("Player Gold: {}".format(player.gold))
+            treasure.destroy()
+            treasures.remove(treasure)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    #Update screen
+    wn.update()
