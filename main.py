debf8bdf8cf245ebc1b51f1c64dc0a1f3448bab4
Swansong101 <owensnicole03@gmail.com>
2023-11-23 12:53:14 +0300

    Eighth commit

--- main.py
+++ main.py
@@ -53,14 +53,17 @@ def setup_maze(level):
                 pen.stamp()
                 walls.append((screen_x, screen_y))
 
-            if character == "P":
+            elif character == "P":
                 player.goto(screen_x, screen_y)
 
-            if character == "T":
+            elif character == "T":
                 treasures.append(Treasure(screen_x, screen_y))
 
-            if character == "E":
+            elif character == "E":
                 enemies.append(Enemy(screen_x, screen_y))
+
+            elif character == "D":  # Add a door
+                doors.append(Door(screen_x, screen_y))  # Create Door instance
 
 
 # Call the function to display the splash screen
@@ -69,11 +72,11 @@ display_splash_screen()
 wn = turtle.Screen()
 wn.bgcolor("black")
 wn.title("Glow In The Dark")
-wn.setup(700,700)
+wn.setup(700, 700)
 wn.tracer(0)
 
-#Register shapes
-images = ["player_right.gif","player_right.gif","treasure.gif","wall.gif",
+# Register shapes
+images = ["player_right.gif", "player_right.gif", "treasure.gif", "wall.gif",
           "pennywise - v2.gif"]
 for image in images:
     turtle.register_shape(image)
@@ -81,7 +84,8 @@ for image in images:
 # Declare reflectors as a global variable
 reflectors = []
 
-#Create path
+
+# Create path
 class Pen(turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self)
@@ -89,6 +93,7 @@ class Pen(turtle.Turtle):
         self.color("green")
         self.penup()
         self.speed(0)
+
 
 class Player(turtle.Turtle):
     def __init__(self):
@@ -100,11 +105,11 @@ class Player(turtle.Turtle):
         self.gold = 0
 
     def go_up(self):
-        #Calculate the space to move to
+        # Calculate the space to move to
         move_to_x = self.xcor()
         move_to_y = self.ycor() + 24
 
-        #Check if the space has a wall
+        # Check if the space has a wall
         if (move_to_x, move_to_y) not in walls:
             self.goto(move_to_x, move_to_y)
 
@@ -140,14 +145,15 @@ class Player(turtle.Turtle):
             self.goto(move_to_x, move_to_y)
 
     def is_collision(self, other):
-        a = self.xcor()-other.xcor()
-        b = self.ycor()-other.ycor()
+        a = self.xcor() - other.xcor()
+        b = self.ycor() - other.ycor()
         distance = math.sqrt((a ** 2) + (b ** 2))
 
         if distance < 5:
             return True
         else:
             return False
+
 
 class Treasure(turtle.Turtle):
     def __init__(self, x, y):
@@ -163,6 +169,7 @@ class Treasure(turtle.Turtle):
         self.goto(2000, 2000)
         self.hideturtle()
 
+
 class Enemy(turtle.Turtle):
     def __init__(self, x, y):
         turtle.Turtle.__init__(self)
@@ -172,9 +179,23 @@ class Enemy(turtle.Turtle):
         self.speed(0)
         self.gold = 25
         self.goto(x, y)
-        self.direction = random.choice(["up","down","left","right"])
+        self.direction = random.choice(["up", "down", "left", "right"])
+        self.move()
 
     def move(self):
+        pass
+
+
+class Door(turtle.Turtle):
+        def __init__(self, x, y):
+            turtle.Turtle.__init__(self)
+            self.shape("door.gif")  # Adjust the shape name according to your door image
+            self.color("brown")  # Adjust the color as needed
+            self.penup()
+            self.speed(0)
+            self.goto(x, y)
+
+def move(self):
         if self.direction == "up":
             dx = 0
             dy = 24
@@ -193,8 +214,8 @@ class Enemy(turtle.Turtle):
             dx = 0
             dy = 0
 
-            #Check if player is close
-            #If so, go in that direction
+            # Check if player is close
+            # If so, go in that direction
         if self.is_close(player):
             if player.xcor() < self.xcor():
                 self.direction = "left"
@@ -205,22 +226,22 @@ class Enemy(turtle.Turtle):
             elif player.ycor() > self.ycor():
                 self.direction = "up"
 
-        #Calculate the spot to move to
+        # Calculate the spot to move to
         move_to_x = self.xcor() + dx
         move_to_y = self.ycor() + dy
 
-        #Check if the space has a wall
+        # Check if the space has a wall
         if (move_to_x, move_to_y) not in walls:
-            self.goto(move_to_x,move_to_y)
+            self.goto(move_to_x, move_to_y)
         else:
-            #Choose a different direction
-            self.direction = random.choice(["up","down","left","right"])
-        #Set timer to move to next time
-        turtle.ontimer(self.move, t=random.randint(100,300))
+            # Choose a different direction
+            self.direction = random.choice(["up", "down", "left", "right"])
+        # Set timer to move to next time
+        turtle.ontimer(self.move, t=random.randint(100, 300))
 
-    def is_close(self, other):
-        a = self.xcor()-other.xcor()
-        b = self.ycor()-other.ycor()
+def is_close(self, other):
+        a = self.xcor() - other.xcor()
+        b = self.ycor() - other.ycor()
         distance = math.sqrt((a ** 2) + (b ** 2))
 
         if distance < 75:
@@ -228,16 +249,15 @@ class Enemy(turtle.Turtle):
         else:
             return False
 
-    def destroy(self):
-        self.goto(2000,2000)
+def destroy(self):
+        self.goto(2000, 2000)
         self.hideturtle()
 
 
-
-#Create levels list
+# Create levels list
 level = [""]
 
-#Define first level
+# Define first level
 level_1 = [
     "XXXXXXXXXXXXXXXXXXXXXXXXX",
     "XP XXXXXXE          XXXXX",
@@ -266,28 +286,29 @@ level_1 = [
     "XXXXXXXXXXXXXXXXXXXXXXXXX"
 ]
 
-#Add a treasure list
+# Add a treasure list
 treasures = []
-#Add enemies list
+# Add enemies list
 enemies = []
-#Add maze to mazes list
+# Add maze to mazes list
 level.append(level_1)
 
-#Create level setup function
+
+# Create level setup function
 def setup_maze(level):
     for y in range(len(level)):
         for x in range(len(level[y])):
-            #Get the character at each x,y coordinate
+            # Get the character at each x,y coordinate
             character = level[y][x]
-            #Calculate the screen x,y coordinates
+            # Calculate the screen x,y coordinates
             screen_x = -288 + (x * 24)
             screen_y = 288 - (y * 24)
-            #Check if it an X
-            if character =="X":
+            # Check if it an X
+            if character == "X":
                 pen.goto(screen_x, screen_y)
                 pen.shape("wall.gif")
                 pen.stamp()
-                #Add coordinates to wall list
+                # Add coordinates to wall list
                 walls.append((screen_x, screen_y))
 
             if character == "P":
@@ -315,46 +336,51 @@ def setup_maze(level):
                     enemies.append(Enemy(screen_x, screen_y))
 
 
-
-#Create class instances
+# Create class instances
 pen = Pen()
 player = Player()
 
-#Create wall coordinates
+# Create wall coordinates
 walls = []
 
-#Set up the level
+# Add a door list
+doors = []
+
+# Set up the level
 setup_maze(level[1])
-print (walls)
+print(walls)
 
-#Keyboard Binding
+# Keyboard Binding
 turtle.listen()
-turtle.onkey(player.go_left,"Left")
-turtle.onkey(player.go_right,"Right")
-turtle.onkey(player.go_up," Up")
-turtle.onkey(player.go_down,"Down")
+turtle.onkey(player.go_left, "Left")
+turtle.onkey(player.go_right, "Right")
+turtle.onkey(player.go_up, " Up")
+turtle.onkey(player.go_down, "Down")
 
-#Turn off screen updates
+# Turn off screen updates
 wn.tracer(0)
 
-#Start moving enemies
+# Start moving enemies
 for enemy in enemies:
     turtle.ontimer(enemy.move, t=250)
 
-
-#Main game loop
+# Main game loop
 while True:
-    #Iterate through treasure list
+    # Iterate through treasure list
     for treasure in treasures:
         if player.is_collision(treasure):
             player.gold += treasure.gold
-            print ("Player Gold: {}".format(player.gold))
+            print("Player Gold: {}".format(player.gold))
             treasure.destroy()
             treasures.remove(treasure)
-    #Iterate through enemy list to see if the player collides
+    # Iterate through enemy list to see if the player collides
     for enemy in enemies:
         if player.is_collision(enemy):
             print("Player Dies!")
-
-    #Update screen
+    # Iterate through door list
+    for door in doors:
+        if player.is_collision(door):
+            print("Player found the door!")
+            # Add any specific actions for when the player finds the door
+    # Update screen
     wn.update()
